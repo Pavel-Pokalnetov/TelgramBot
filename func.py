305b12324679f617d4d722df7d8b6b5b30377323
Pokalnetov Pavel <pokalnetovps@gmail.com>
2023-01-28 18:31:59 +0500

    фото результата

--- func.py
+++ func.py
@@ -10,7 +10,6 @@ def days2NY():
     now = datetime.today()
     NY = datetime(now.year + 1, 1, 1)
     d = NY-now
-
     mm, ss = divmod(d.seconds, 60)
     hh, mm = divmod(mm, 60)
     return ('До нового года: {} дней {} часа {} мин {} сек.'.format(d.days, hh, mm, ss))
@@ -29,3 +28,7 @@ def getweather():
         return (weather)
     except Exception as e:
         print("Exception (weather):", e)
+
+
+if __name__=='__main__':
+    getweather()
