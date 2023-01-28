96dedb5d5dc47f6468744d5eec6008fbfc88ccb4
Pokalnetov Pavel <pokalnetovps@gmail.com>
2023-01-28 21:28:18 +0500

    add class Game (игра в 50-8 спичек)

---
+++ game.py
@@ -0,0 +1,74 @@
+class Game:
+    '''
+    игра спички
+    на столе 50 спичек
+    за ход можно забрать не более 8 спичек
+    выигрывает то, забрал последние спички со стола
+    '''
+
+    heap: int  # куча спичек
+    gamestatus: bool  # состояние игры
+
+    def __init__(self):
+        self.gamestatus = False
+        self.heap = 50
+
+    def action_player(self, count_items):
+        '''
+        возвращает результат:
+            -1 - неверное количество спичек
+             0 - игрок сделал ход, игра продолжается
+             1 - игрок выиграл
+
+        '''
+        
+        if self.gamestatus:
+            if count_items < 0 or count_items > 8:
+                # игрок взял неверное число спичек
+                return [-1]
+            elif self.heap-count_items == 0:
+                # игрок выиграл
+                self.heap = 0
+                return [1]
+            elif self.heap-count_items < 0:
+                # игрок выбрал больше чем есть
+                pass
+                return [-1]
+            else:
+                # игрок сделал ход
+                self.heap -= count_items
+                return [0]
+        else:
+            pass
+
+    def action_cpu(self):
+        
+        if self.gamestatus:
+            if self.heap <= 8:
+                # компьютер выиграл
+                count = self.heap
+                self.heap = 0
+                return [1, count]
+            elif self.heap == 9:
+                # компьютер проиграл
+                count = random.randint(1, 8)
+                self.heap -= count
+                return [0, count]
+            elif self.heap-9 <= 8:
+                # предвыигрышный ход
+                count = self.heap-9
+                self.heap -= count
+                return [0, count]
+            else:
+                # обычный ход
+                count = random.randint(1, 8)
+                self.heap -= count
+                return [0, count]
+        else:
+            pass
+
+    def check_game_state(self):
+        return self.heap
+    
+    
+        
