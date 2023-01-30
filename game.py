d45604cf65cf5e76e820056a3d4cb5edd9575cbd
Pokalnetov Pavel <pokalnetovps@gmail.com>
2023-01-31 00:59:42 +0500

    обработка сообщений вне команд

--- game.py
+++ game.py
@@ -1,7 +1,11 @@
+import enum
+
+
 class Order(enum.Enum):
     '''кто ходит'''
     player = 0
     cpu = 1
+
 
 class Game:
     '''
@@ -16,18 +20,21 @@ class Game:
 
     def __init__(self):
         self.gamestatus = False
+        '''состояние игрв (продолжается)'''
         self.heap = 50
+        '''число спичек в куче'''
         self.act = Order.player
-        
+        '''чей ход
+        cpu
+        player'''
+
     def action_player(self, count_items):
-        '''
+        ''' ход игрока
         возвращает результат:
             -1 - неверное количество спичек
              0 - игрок сделал ход, игра продолжается
              1 - игрок выиграл
-
         '''
-        
         if self.gamestatus:
             if count_items < 0 or count_items > 8:
                 # игрок взял неверное число спичек
@@ -48,18 +55,13 @@ class Game:
             pass
 
     def action_cpu(self):
-        
+        '''ход компьютера'''
         if self.gamestatus:
             if self.heap <= 8:
                 # компьютер выиграл
                 count = self.heap
                 self.heap = 0
                 return [1, count]
-            elif self.heap == 9:
-                # компьютер проиграл
-                count = random.randint(1, 8)
-                self.heap -= count
-                return [0, count]
             elif self.heap-9 <= 8:
                 # предвыигрышный ход
                 count = self.heap-9
@@ -73,16 +75,48 @@ class Game:
         else:
             pass
 
-    def check_game_state(self):
-        return self.heap
-    
-    def tik(self):
+    def check_game_state():
+        '''проверка на конец игры (0 спичек)'''
+        return self.heap == 0
+
+    def tik(self, count_matches_player):
         '''циклический ход игры'''
-        if act==Order.cpu:
-            # ход компьютера
-            pass
-        else:
-            # ход игрока
-            pass
-        
-        
+        def matches(number):
+            '''
+            1 - спичка
+            2,3,4 - спички
+            5,6,7,8 - спичек
+            '''
+            result = str(number) + ' спич'
+            if number == 1:
+                result += 'ка'
+            elif 1 < number < 5:
+                result += 'ки'
+            else:
+                result += 'ек'
+            return result
+
+        if (self.gamestatus()):
+            if act == Order.cpu:
+                # ход компьютера
+                state = self.action_cpu()
+                if state[0] == 1:
+                    # компьютер выиграл
+                    self.gamestatus = False
+                    return ('Ход компьютера - {},\n'
+                            'осталось 0 спичек,\n'
+                            'Вы проиграли'.format(matches(state[1])))
+                else:
+                    return ('Ход компьютера - {},\n'
+                            'осталось {} спичек,\n'
+                            'Ваш ход'.format(matches(state[1]), self.heap))
+                act = Order.player
+            else:
+                # ход игрока
+                state = self.action_player(count_matches_player)
+                if state[0] == '1':
+                    # игрок выиграл
+                    self.gamestatus == False
+                    return ("Вы выиграли")
+                elif state[0] == '0':
+                    pass
--- runbot.py
+++ runbot.py
@@ -1,5 +1,5 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes,MessageHandler
 
 from config import TOKEN
 from func import calcrun, days2NY, getweather
@@ -31,6 +31,10 @@ async def day2NewYear(update: Update, context: ContextTypes.DEFAULT_TYPE) -> Non
 async def getweath(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'{getweather()}')
 
+async def message_processing(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    if update.message.text[0] !='/': 
+        await update.message.reply_text('Просто текст')
+
 if __name__ == '__main__':
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("GW", getweath))
@@ -38,4 +42,7 @@ if __name__ == '__main__':
     app.add_handler(CommandHandler("calc", calc))
     app.add_handler(CommandHandler("echo", echo))
     app.add_handler(CommandHandler("D2NY", day2NewYear))
+    
+    app.add_handler(MessageHandler(None,message_processing))
+    
     app.run_polling()
